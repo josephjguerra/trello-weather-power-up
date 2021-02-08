61e01414d7b92ca8e0a52fa2c0a544b0c0c35d9c
josephjguerra <joseph.j.guerra@gmail.com>
2021-02-08 16:47:41 -0500

    add card details badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,5 +1,5 @@
 window.TrelloPowerUp.initialize({
-  "card-badges": function (t, opts) {
+  "card-badges": function (t) {
     // return array of card badges for the given card
     return t.card('coordinates')
     .then(function(card) {
@@ -18,6 +18,34 @@ window.TrelloPowerUp.initialize({
           text: `🌬 ${Math.round(weatherData.wind.speed)} mph`
         },{
           icon: `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
+          text: weatherData.weather[0].main
+        }]
+      });
+    }
+    return [];
+    })
+  },
+  "card-detail-badges": function(t) {
+    // return array of card badges for the given card
+    return t.card('coordinates')
+    .then(function(card) {
+      console.log(card);
+    if (card.coordinates) {
+      const { latitude, longitude } = card.coordinates;
+      // card has location, so let's fetch the current weather
+      return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%&units=imperial`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(weatherData) {
+        return[{
+          title: 'Temperature',
+          text: `${Math.round(weatherData.main.temp)}°F`
+        },{
+          title: 'Winds',
+          text: `🌬 ${Math.round(weatherData.wind.speed)} mph`
+        },{
+          title: 'Weather',
           text: weatherData.weather[0].main
         }]
       });
