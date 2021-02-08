c52c7352829fb78059098130fd9a1e2ac61687f9
josephjguerra <joseph.j.guerra@gmail.com>
2021-02-08 13:38:59 -0500

    fetch weather data for card badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,9 +1,25 @@
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
     // return array of card badges for the given card
-    return t.card('all')
+    return t.card('coordinates')
     .then(function(card) {
       console.log(card);
+    if (card.coordinates) {
+      const { latitude, longtidue } = card.coordinates;
+      // card has location, so let's fetch the current weather
+      return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtidue}&appid=%%APP_ID%%`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(weatherData) {
+        return[{
+          text: weatherData.main.temp.toString()
+        },{
+          text: weatherData.wind.speed.toString()
+        }]
+      });
+    }
+    return [];
     })
   }
 });
